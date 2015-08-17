089a63fa0d7f568095ef4e0be564cf599396a030
jfurness <pcxjf1@nottingham.ac.uk>
2015-08-17 17:17:39 +0100

    JWF: Committing version 1

---
+++ Solver.py
@@ -0,0 +1,382 @@
+import math
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize_scalar
+import sys
+
+def convertUto2ReducedAU(mu, Ulist, deinf):
+    for i in range(0,len(Ulist)):
+        Ulist[i] *= 2*mu
+    
+    deinf *= 2*mu
+    return Ulist, deinf
+    
+def revertFrom2ReducedAU(mu, Ulist, deinf):
+    for i in range(0,len(Ulist)):
+        Ulist[i] /= 2*mu
+    
+    deinf /= 2*mu
+    return Ulist, deinf
+    
+def enToEh(mu, en):
+    return en/(2*mu)
+
+def getFloat(inp, lineNo):
+    try:
+        out = float(inp)
+    except ValueError:
+        print "INPUT ERROR: line ",lineNo
+        print "Unable to read expected float."
+        sys.exit()
+    return out
+
+def readInput(inpname):
+    try:
+        finput = open(inpname,'r')
+    except IOError:
+        print "ERROR: Failed to find file " + inpname
+        sys.exit()    
+            
+    Rlist = []
+    Ulist = []
+    re = None
+    de = None
+    
+    M1 = None
+    M2 = None
+    
+    lineNo = 0
+    
+    readingData = False
+    
+    for line in finput:
+        lineNo += 1
+        
+        if (not readingData):
+            if (line.find("[") != -1):
+                readingData = True
+                if (len(line.strip()) > 1):
+                    print "INPUT ERROR: New line before data please! line", lineNo
+                    sys.exit()
+            else:
+                line = line.upper()
+                parts = line.strip().split('=')
+                
+                if (len(parts) != 2):
+                    print "INPUT ERROR: I don't understand line", lineNo 
+                    sys.exit()
+                    
+                field = parts[0].strip()
+                val = parts[1].strip()
+                
+                
+                if (field == "D0"):
+                    de = getFloat(val, lineNo)
+                        
+                #elif (field == "RE"):
+                #    re = getFloat(val, lineNo)
+                
+                elif (field == "M1"): 
+                    M1 = getFloat(val, lineNo)
+                    
+                elif (field == "M2"):
+                    M2 = getFloat(val, lineNo)
+                
+                else:
+                    print "WARNING: I don't understand the field " + field + " on line ", lineNo
+                
+        else:
+            if (line.find("]") != -1):
+                readingData = False
+                if (len(line.strip()) > 1):
+                    print "INPUT ERROR: New line before more input please! line", lineNo
+                    sys.exit()
+            else:
+                line = line.strip().split(',')
+                line = filter(None, line)
+                
+                if (len(line) != 2):
+                    print "INPUT ERROR: Bad data line ", lineNo
+                    print "Comma separated values for data please. e.g."
+                    print "\tseparation, energy"
+                    sys.exit()
+    
+                line[0] = line[0].strip()
+                line[1] = line[1].strip()            
+                
+                # Compensate for mathematica's annoying lack of 0. e.g. "1." for "1.0"
+                if (line[0][-1] == '.'):
+                    line[0] = line[0] + '0'
+                if (line[1][-1] == '.'):
+                    line[0] = line[0] + '0'
+                    
+                Rlist.append(getFloat(line[0], lineNo))
+                Ulist.append(getFloat(line[1], lineNo))
+        
+    print "Potential consists of ", len(Ulist)," points."
+    
+    if re is None:
+        re = Ulist.index(min(Ulist))
+    
+    if de is None:
+        print "WARNING: Guessing at D0 as no input value found."
+        print "         This may be inaccurate if potential is not long ranged enough."
+        de = Ulist[-1]
+    
+    if M1 is None:
+        print "INPUT ERROR: No mass found for atom 1.  e.g. M1 = 1.0"
+        sys.exit()
+    if M2 is None:
+        print "INPUT ERROR: No mass found for atom 2.  e.g. M2 = 1.0"
+        sys.exit()
+        
+    # Convert potential to be 0 at Re. (Currently always does this as no fitting is done).
+    setUtoRe(Ulist)
+    
+    
+    # Determine a.u. reduced mass for potential scaling
+    mu = reducedMass(convertMassToAU(M1), convertMassToAU(M2))
+    
+    # REMEMBER! THE MORSE POTENTIAL FOR COOLEY'S REFERENCE HAS THIS BUILT IN. SET MU to 0.5
+    # deinf = 0.16454007507   # AND SET THIS TO 188.4355
+    
+    # Convert potential and de to a.u. scaled reduced mass 
+    Ulist, de = convertUto2ReducedAU(mu, Ulist, de)
+   
+    return Rlist, Ulist, mu, M1, M2, abs(de)
+
+def convertMassToAU(M):
+    return M*1822.88839
+
+def reducedMass(M1, M2):
+    return M1*M2/(M1+M2)
+    
+def rotationPotential(Rlist, Ulist, J, lam):
+    for i in range(0,len(Rlist)):
+        if Rlist[i] != 0:
+            Ulist[i] += (J*(J+1) - lam**2)/Rlist[i]**2
+        else:
+            Ulist[i] = 1E12
+    return Ulist
+
+def setUtoRe(Ulist):        
+    smallest = min(Ulist)
+
+    for i in range(0,len(Ulist)):
+        Ulist[i] -= smallest
+        
+def setUtoDe(Ulist, deinf):
+    for i in range(0,len(Ulist)):
+        Ulist[i] -= deinf
+
+def morse(alpha, re, de, r):
+    return de*(1.0 - math.exp(-alpha*(r - re)))**2
+
+def morseResidual(Rlist, Ulist, re, de, alpha):
+    sumOSqrs = 0
+    for i in range(0, len(Rlist)):
+        if (Ulist[i] <= de):
+            sumOSqrs += (morse(alpha, re, de, Rlist[i]) - Ulist[i])**2
+    return sumOSqrs
+    
+def plotFitting(calc, alpha):
+    # Generate list of morse points
+    calc.morseList = []
+    for r in calc.Rlist:
+        calc.morseList.append(morse(alpha, calc.Re, calc.D0, r))
+    calc._toInterest_fired()
+    calc.plotRU()
+    #fig = plt.figure()
+    #Uplot = fig.add_subplot(111)
+    #Uplot.set_xlim([min(Rlist), max(Rlist)])
+    #Uplot.set_ylim([min(Ulist), 1.1*de])
+    #
+    #Uplot.plot(Rlist, Ulist, color='green', label="Input Potential")
+    #Uplot.plot(Rlist, morseList, color='red', label="Fit Morse", linestyle='--')
+    #
+    #Uplot.set_xlabel("Separation ($r_0$)")
+    #Uplot.set_ylabel("Reduced Mass Scaled Potential (2*Eh*mu)")
+    #Uplot.legend()
+    #
+    #Uplot.set_title("Fit \nAlpha: "+str(alpha))
+    
+    #fig.show()
+    
+
+def assessFit( alpha, Rlist, Ulist, re, de):
+    return morseResidual(Rlist, Ulist, re, de, alpha)
+
+def fitMorse(calc):
+    
+    result =  minimize_scalar(assessFit, args=(calc.Rlist, calc.Ulist, calc.Re, calc.D0))
+    
+    alpha = result.x
+    calc.add_line("Morse potential fit for starting guess")
+
+    calc.add_line("Alpha: "+str(alpha))
+    calc.add_line("Sum of squares: "+str(assessFit(alpha, calc.Rlist, calc.Ulist, calc.Re, calc.D0)))
+    
+    
+    # TODO:  Convert this to use calc plotting!
+    plotFitting(calc, alpha)
+    return alpha
+
+def morseEigs(calc, alpha):
+    # mass enters as reduced mass in Daltons for agreement with Cooley 
+    eig = 0
+    calc.guessEigList = []
+    k = int((4*math.pi*math.sqrt(2*calc.muma*calc.D0)/(2*math.pi*alpha)-1)/2.0)
+
+    calc.add_line("# Eigenstates: " + str(k))
+    
+    calc.add_line("Morse Eigenvalues", True)
+
+    n = 0
+    while  n < min(k,50):
+        omega0 = (alpha/(2*math.pi))*math.sqrt(2*calc.D0/calc.muma)
+        eig = -calc.D0 + 2*math.pi*omega0*(n+0.5) - ((2*math.pi)**2*omega0**2) / (4.0*calc.D0) * (n+0.5)**2
+        calc.guessEigList.append(eig)
+        
+        calc.add_line("v"+str(n)+": "+str(eig))
+        n += 1
+    return calc.guessEigList
+        
+def generateStartingE(calc):
+    alpha = fitMorse(calc)     
+    return morseEigs(calc, alpha)
+
+def nextPout(i, Rlist, Ulist, E, h, Plist):    
+    if False:   # Simple Integration formula
+        return h**2*(Ulist[i] - E)* Plist[i] + 2.0*Plist[i] - Plist[i-1]
+    else:
+        Yim1 = (1 - (h**2/12.0) * (Ulist[i-1] - E))*Plist[i-1]
+        Yi = (1 - (h**2/12.0) * (Ulist[i] - E))*Plist[i]
+        num = h**2*(Ulist[i] - E) * Plist[i] + 2*Yi - Yim1
+        return num / (1 - (h**2/12.0)*(Ulist[i+1]-E))
+        
+def nextPin(i, Rlist, Ulist, E, h, Plist):
+    if False:   # Simple Integration formula
+        return h**2*(Ulist[i]-E)*Plist[i] + 2.0*Plist[i] - Plist[i+1]
+    else:
+        Yip1 = (1 - (h**2/12.0) * (Ulist[i+1] - E))*Plist[i+1]
+        Yi = (1 - (h**2/12.0) * (Ulist[i] - E))*Plist[i]
+        num = h**2*(Ulist[i] - E) * Plist[i] + 2*Yi - Yip1
+        return num / (1 - (h**2/12.0)*(Ulist[i-1]-E))
+        
+def integrate(calc, Rlist, Ulist, E, h):
+    if (E > Ulist[-1]):
+        calc.add_line( "Unbound state!")
+        return "UNBOUND"
+    
+    # Integrate inwards
+    Pin = [0.0]*(len(Ulist))
+    
+
+    Pin[-1] = 1.0
+    Pin[-2] = Pin[-1]*math.exp( Rlist[-1]*math.sqrt( Ulist[-1] - E ) - Rlist[-2]*math.sqrt(Ulist[-2] - E) )
+    
+    m = -1
+    
+    for i in reversed(range(0,len(Pin)-2)):
+        Pin[i] = nextPin(i+1, Rlist, Ulist, E, h, Pin)
+        
+        if (Pin[i] < Pin[i+1]):
+            Pin[i] = 0.0
+            m = i+1
+            break
+
+    if (m == -1):
+        calc.add_line("ERROR in integration!")
+        calc.add_line("Inward integration never hits maximum.")
+        return "ERROR"
+    
+    Pout = [0.0]*len(Rlist)
+    Pout[0] = 0
+    Pout[1] = 1.0
+    for i in range(2,m+1):
+        Pout[i] = nextPout(i-1, Rlist, Ulist, E, h, Pout)
+    
+    return Pout, Pin, m
+    
+def normaliseIntegration(Pout, Pin, m):
+    for i in reversed(range(0,len(Pin))):
+        Pin[i] /= Pin[m]
+
+    for i in range(0,len(Pout)):
+        Pout[i] /= Pout[m]
+    
+    return Pout, Pin
+    
+def getDerivs(Pout, Pin, Rlist, m):
+    dPout = (Pout[m-1] - Pout[m]) / (Rlist[m-1] - Rlist[m])
+    dPin = (Pin[m] - Pin[m+1]) / (Rlist[m] - Rlist[m+1])
+    return dPout, dPin
+    
+def integrateP2(Pout, Pin, Rlist, m):
+    total = 0.0
+    for i in range(0, len(Rlist)-1):
+        if i < m:
+            P2 = ((Pout[i] + Pout[i+1])/2.0)**2
+        else:
+            P2 = ((Pin[i] + Pin[i+1])/2.0)**2
+        total += P2 * (Rlist[i+1] - Rlist[i])
+    return total
+    
+def simpleCorrectE( Pout, Pin, Rlist, m):
+    dPout, dPin = getDerivs(Pout, Pin, Rlist, m)
+    
+    return (dPout - dPin) / integrateP2(Pout, Pin, Rlist, m)
+    
+def correctE( Pout, Pin, Ulist, E, h, m):
+    sumP2 = 0.0
+    for i in range(0, len(Pout)):
+        if i < m:
+            sumP2 += Pout[i]**2
+        else:
+            sumP2 += Pin[i]**2
+
+    Ymm1 = (1 - (h**2/12.0) * (Ulist[m-1] - E))*Pout[m-1]
+    Ym = (1 - (h**2/12.0) * (Ulist[m] - E))*Pout[m]
+    Ymp1 = (1 - (h**2/12.0) * (Ulist[m+1] - E))*Pin[m+1]
+    yBit = ((-Ymm1 + 2*Ym - Ymp1)/h**2)
+    return (yBit + (Ulist[m] - E)*Pout[m])/sumP2
+    
+
+def optimizeEigenvalue(calc, Rlist, Ulist, E, h):
+    calc.add_line("Initial Guess: "+str(E))
+    
+    dE = 1E12
+    tol = 1E-6
+    
+    it = 0
+    while (abs(dE) >= tol):
+        try:
+            Pout, Pin, m = integrate(calc, Rlist, Ulist, E, h)
+            Pout, Pin = normaliseIntegration(Pout, Pin, m)
+            
+            #if False:
+            #    Pfig = plt.figure()
+            #    Pplot = Pfig.add_subplot(111)
+            #    Pplot.set_xlim([min(Rlist), max(Rlist)])
+            #    Pplot.set_ylim([min([min(Pin),min(Pout)]), max([max(Pin),max(Pout)])])
+            #    
+            #    Pplot.set_title("Iteration: " +str(it))
+            #    Pplot.plot(Rlist[:m], Pout[:m], color='green')
+            #    Pplot.plot(Rlist[m:], Pin[m:], color='red')
+            #    
+            #    Pfig.show()
+            
+            dE = correctE( Pout, Pin, Ulist, E, h, m)
+            
+            E += dE
+            
+            calc.add_line("\n-----------\n"+"Iteration "+str(it)+"\n-----------")
+            calc.add_line("Correction: "+str(dE))
+            calc.add_line("New E: "+str(E))
+            
+            it += 1
+        except ValueError as e:
+            return "UNBOUND"
+        
+    calc.add_line( "\n\nConverged! At: "+str(E))
+    calc.add_line("With dE: "+str(dE))
+    return E, Pout[:m]+Pin[m:]
---
+++ vibr-o-matic.py
@@ -0,0 +1,329 @@
+# -*- coding: utf-8 -*-
+from enthought.traits.api import *
+from enthought.traits.ui.api import *
+from chaco.api import ArrayPlotData, Plot, PlotGraphicsContext
+from enable.component_editor import ComponentEditor
+from pyface.api import FileDialog
+
+from Solver import *
+
+class Calculation(HasTraits):
+    M1 = Float()
+    M2 = Float()
+    D0 = Float()
+    Re = Float()
+    levels = String("0,1,2-6")
+    levelsToFind = []
+    loadPot = Button()
+    
+    muma = 0.0
+    mu = 0.0
+    
+    solve = Button()
+    
+    plot = Instance(Plot)
+    toInterest = Button()
+    
+    results = String()
+    saveLog = Button()
+    
+    savePlot = Button()
+    
+    scaled = False
+    
+    Rlist = []
+    Ulist = []
+    
+    plotRangeX = []
+    plotRangeY = []
+    
+    self.h = 0  # The separation between R points
+    
+    morseList = []
+    guessEigList = []
+    convergedValues = []
+    Plists = []
+    
+    view = View(HSplit(
+                    Group(
+                        Item('loadPot',show_label=False, label="\nLoad Potential\n"),
+                        Item(name='M1'),
+                        Item(name='M2'),
+                        Item(name='D0'),
+                        Item(name='Re'),
+                        Item(name='levels'),
+                        Item('solve', show_label=False, label="\nSolve the System!\n")
+                    ),
+                    Group(
+                        Item(name='results', show_label=False,style='custom'),
+                        Item(name='saveLog', show_label=False,label="Save output")
+                    ),
+                    Group(
+                        Item('plot', editor = ComponentEditor(), show_label=False),
+                        Item('toInterest',show_label=False, label="Scale Plot"),
+                        Item('savePlot',show_label=False, label="Save Plot")
+                    )
+                ),
+                title = "Vibr-o-matic",
+                width = 800,
+                height = 400,
+                resizable = True)
+                
+
+    def __init__(self):
+        self.greeting()
+        
+    def _loadPot_fired(self):
+        self.Rlist = []
+        self.Ulist = []
+        self.morseList = []
+        self.guessEigList = []
+        self.convergedValues = []
+        self.Plists = []
+        self.muma = 0.0
+        self.mu = 0.0
+        try:
+            dialog = FileDialog(title='Select a potential',action='open')
+            if not dialog.open():
+                self.add_line("I can't open the file \'" + str(dialog.path)+"\'")
+                return
+            inp = open(dialog.path,'r')
+            self.potential = []
+            for line in inp:
+                raw = line.strip().split(',')
+                raw = filter(None, raw)
+                
+                try:
+                    R = float(raw[0])
+                    U = float(raw[1])
+                    self.Rlist.append(R)
+                    self.Ulist.append(U)
+                    
+                except ValueError:
+                    self.add_line("I cant convert this to two floats:")
+                    self.add_line("\""+str(line)+"\"")
+            
+            self.plotRU()
+            self.add_line("Loading new potential!",True)
+            self.add_line("Loading potential from: "+str(inp.name))
+        except IOError:
+            self.add_line("I can't find the file \'" + str(dialog.path)+"\'")
+    
+    def _toInterest_fired(self):
+        self.D0=abs(self.D0)
+        if len(self.Rlist) == 0:
+            self.add_line("Load a potential first!")
+            return
+        if self.D0 == 0:
+            self.add_line("Set a D0 first!")
+            return
+        if (min(self.Ulist) < 0):
+            if len(self.morseList) > 0 and min(self.morseList) >= 0:
+                setUtoDe(self.morseList, self.D0)
+            self.plotRangeY = (-self.D0, self.D0/10.0)
+        else:
+            self.plotRangeY = (0, self.D0*1.1)
+        self.plotRU()
+    
+    def _savePlot_fired(self):
+        dialog = FileDialog(title='Save plot as...', action='save as',
+                            wildcard = FileDialog.create_wildcard('Portable Network Graphics', '*.png'))
+        if not dialog.open():
+            self.add_line("ERROR opening save file.")
+            return
+        self.plotRU(save=True,filename=dialog.path)
+    
+    def _saveLog_fired(self):
+        dialog = FileDialog(title='Save plot as...', action='save as',
+                            wildcard = FileDialog.create_wildcard('Text file', '*.txt'))
+        if not dialog.open():
+            self.add_line("ERROR opening save file.")
+            return
+        try:
+            out = open(dialog.path,'w')
+            out.write(self.results)
+            out.close()
+        except IOError:
+            self.add_line("I failed to open "+str(dialog.path))
+            return
+            
+    def _solve_fired(self):
+        # Parse the levels asked for. Return False on error.
+        if not self.parseLevels():
+            return
+        self.add_line("Beginning solver",title=True)
+            
+        if self.dataMissing():
+            return
+    
+        if self.Re == 0:
+            self.add_line("WARNING! : No Re set. Guessing from potential!")
+            self.Re = self.Rlist[self.Ulist.index(min(self.Ulist))]
+            self.add_line("Re set as: "+str(self.Re))
+        
+        setUtoRe(self.Ulist)
+        self._toInterest_fired()
+    
+        self.muma = reducedMass(self.M1, self.M2)
+        self.mu = reducedMass(convertMassToAU(self.M1), convertMassToAU(self.M2))
+        self.add_line("Converting Potential and D0 to 2 x reduced mass scale.")
+        self.add_line("Reduced mass (au) = "+str(self.mu))
+        self.add_line("Reduced mass (ma) = "+str(self.muma))
+        self.Ulist, self.D0 = convertUto2ReducedAU(self.mu, self.Ulist, self.D0)
+        self.scaled = True
+    
+        generateStartingE(self)
+        
+        setUtoDe(self.Ulist, self.D0)
+        
+        if len(self.guessEigList) == 0:
+            self.add_line("No bound Eigenvalues were found for guess potential.")
+            self.add_line("For the hell of it, lets try optimizing something very nearly unbound...")
+
+            # try looking for one right at the dissociation limit.
+            self.guessEigList.append(-self.D0*0.95)
+        
+        self.h = self.Rlist[-1] - self.Rlist[-2]      # This is the separation between R points
+
+        self.add_line("\nR Points Separation: "+str(self.h))
+
+        self.convergedValues = [0.0]*len(self.guessEigList)
+        for val in self.levelsToFind:
+            if val >= len(self.guessEigList):
+                self.add_line("\nERROR: No guess state for level "+str(val))
+                self.add_line("Possbily state is unbound, manual guesses to be implimented eventually...") 
+            else:
+                self.add_line("Optimizing level "+str(val),True)
+                self.convergedValues[val], tlist = optimizeEigenvalue(self, self.Rlist, self.Ulist, self.guessEigList[val], self.h)
+                self.Plists.append(tlist)
+        
+        self.Ulist, self.D0 = revertFrom2ReducedAU(self.mu, self.Ulist, self.D0)
+        self.morseList, tmp = revertFrom2ReducedAU(self.mu, self.morseList, 0.0)
+        self.scaled = False
+        
+        for val in self.levelsToFind:
+            if val < len(self.guessEigList):
+                if self.convergedValues[val] != "UNBOUND":
+                    self.convergedValues[val] = enToEh(self.mu, self.convergedValues[val])
+        
+        self._toInterest_fired()
+        
+        self.add_line("Summary",True)
+        # Final Print Roundup
+        for val in self.levelsToFind:
+            if val < len(self.guessEigList):
+                if self.convergedValues[val] != "UNBOUND":
+                    self.add_line("v"+str(val)+": " + str(self.convergedValues[val])+" Eh\t"+str(abs(self.convergedValues[val])*219474.63)+" cm-1")
+                else:
+                    self.add_line("v"+str(val)+" is not a bound state!")
+        
+    
+    def add_line(self, string, title=False):
+        if title:
+            pad = "-"*len(string)
+            self.results += ("\n\n"+pad+"\n"+ string.upper() + "\n"+pad+"\n\n")
+        else:
+            self.results += string + "\n"
+       
+    def plotRU(self,rangeX=None, rangeY=None, save=False, filename=""):
+        if save and filename == "":
+            self.add_line("ERROR: I need a valid file name")
+            return
+        
+        if save and filename.split('.')[-1] != "png":
+            self.add_line("ERROR: File must end in .png")
+            return
+        
+        if len(self.morseList) > 0:
+            plotData = ArrayPlotData(x=self.Rlist, y=self.Ulist, morse=self.morseList, eigX=[self.Rlist[0], self.Rlist[-1]])
+        else:
+            plotData = ArrayPlotData(x=self.Rlist, y=self.Ulist)
+            
+        for val in self.levelsToFind:
+            if val < len(self.guessEigList):
+                plotData.set_data("eig"+str(val), [self.convergedValues[val], self.convergedValues[val]])
+            
+        plot = Plot(plotData)
+        
+        if len(self.morseList) > 0:
+            plot.plot(("x","morse"), type = "line", color = "red")
+        
+        for val in self.levelsToFind:
+            if val < len(self.guessEigList):
+                plot.plot(("eigX","eig"+str(val)), type="line", color="green")
+        
+        plot.plot(("x","y"), type = "line", color = "blue")
+        plot.plot(("x","y"), type = "scatter", marker_size = 1.0, color = "blue")
+        #
+        plot.index_axis.title = "Separation (r0)"
+        if (self.scaled):
+            plot.value_axis.title = "Potential (Eh * 2 * mu)"
+        else:
+            plot.value_axis.title = "Potential (Eh)"
+            
+        if len(self.plotRangeX) != 0:
+            plot.x_axis.mapper.range.low = self.plotRangeX[0]
+            plot.x_axis.mapper.range.high = self.plotRangeX[1]
+        if len(self.plotRangeY) != 0:
+            plot.y_axis.mapper.range.low = self.plotRangeY[0]
+            plot.y_axis.mapper.range.high = self.plotRangeY[1]
+        if not save:
+            self.plot = plot
+        else:
+            plot.outer_bounds = [800,600]
+            plot.do_layout(force=True)
+            gc = PlotGraphicsContext((800,600), dpi = 72)
+            gc.render_component(plot)
+            gc.save(filename)
+
+    def dataMissing(self):
+        fail = False
+        if len(self.Rlist) == 0:
+            self.add_line("ERROR: Load a potential first!")
+            fail = True
+        if self.D0 == 0:
+            self.add_line("ERROR: Set a D0!")
+            fail = True
+        if self.M1 == 0:
+            self.add_line("ERROR: Set a mass for atom 1!")
+            fail = True
+        if self.M2 == 0:
+            self.add_line("ERROR: Set a mass for atom 2!")
+            fail = True
+        return fail
+
+    def parseLevels(self):
+        raw = self.levels.split(',')
+        for item in raw:
+            item = item.strip()
+            if item.find('-') != -1:
+                temp = item.split('-')
+                try:
+                    for i in range(int(temp[0]), int(temp[1])+1):
+                        self.levelsToFind.append(i)
+                except ValueError:
+                    self.add_line("ERROR: Problem in level choice!")
+                    self.add_line("Choose individual levels with comma separation")
+                    self.add_line("or a range with start-stop")
+                    return False
+            else:
+                try:
+                    self.levelsToFind.append(int(item))
+                except ValueError:
+                    self.add_line("ERROR: Problem in level choice!")
+                    self.add_line("Choose individual levels with comma separation")
+                    self.add_line("or a range with start-stop")
+                    return False
+                    
+        return True
+
+    def greeting(self):
+        self.add_line("=============================")
+        self.add_line("        VIBR-O-MATIC")
+        self.add_line("=============================")
+        self.add_line("\nA Diatomic vibrations solver.")
+        self.add_line("By James Furness\n")
+        
+# Read the input file
+inp = Calculation().configure_traits()
+# Rlist, Ulist, mu, M1, M2, deinf = readInput(inpname)
